0560390167bdddd3d7d8cefe1828ee9c666b95f9
Artosach <jpsan.sig@outlook.com>
2025-02-26 18:01:24 +0100

    Update CommandInjection.java
    Comment

--- src/main/java/CommandInjection.java
+++ src/main/java/CommandInjection.java
@@ -4,7 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 public class CommandInjection {
     public static Process runCmd(HttpServletRequest request) throws IOException {
         String filename = request.getParameter("filename");
-        ProcessBuilder builder = new ProcessBuilder("cat", filename);
+        //ProcessBuilder builder = new ProcessBuilder("cat", filename);
+        ProcessBuilder builder = new ProcessBuilder("cat", new File(filename).getCanonicalPath());
         Process process = builder.start();
         return(process);
     }
